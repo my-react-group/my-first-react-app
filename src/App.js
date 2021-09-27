1970bd4180036700e33173b2e7f8c5b48d38592e
litianyi <1019180951@qq.com>
2021-09-27 16:47:06 +0800

    11

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { Switch, Route, Router } from 'react-router-dom';
+import routes from './router/routes';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        {
+          routes.map(item => {
+            return <Route path={item.path} component={item.component}></Route>
+          })
+        }
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/router/routes.js
@@ -0,0 +1,9 @@
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('@/views/index.js')
+  }
+]
+
+export default routes
